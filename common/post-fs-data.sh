7ee750305e5613f5cc2bef2d0eff6f9202d402f6
Rom <skav@gmx.fr>
2018-04-05 09:49:41 +0200

    Added address props to post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,27 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+setprop dhcp.wlan0.dns1 1.1.1.1
+setprop dhcp.wlan0.dns2 1.0.0.1
+
+setprop net.eth0.dns1 1.1.1.1
+setprop net.eth0.dns2 1.0.0.1
+
+setprop setprop net.dns1 1.1.1.1
+setprop setprop net.dns2 1.0.0.1
+
+setprop net.ppp0.dns1 1.1.1.1
+setprop net.ppp0.dns2 1.0.0.1
+
+setprop net.rmnet0.dns1 1.1.1.1
+setprop net.rmnet0.dns2 1.0.0.1
+
+setprop net.rmnet1.dns1 1.1.1.1
+setprop net.rmnet1.dns2 1.0.0.1
+
+setprop net.dns1 1.1.1.1
+setprop net.dns2 1.0.0.1
+
+setprop net.pdpbr1.dns1 1.1.1.1
+setprop net.pdpbr1.dns2 1.0.0.1
