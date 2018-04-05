cacb60f0d5c4b669c13234309bf5ca791eb27db2
Rom <skav@gmx.fr>
2018-04-05 17:03:18 +0200

    Update post-fs-data.sh to 2.4

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,26 +6,34 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-setprop dhcp.wlan0.dns1 1.1.1.1
-setprop dhcp.wlan0.dns2 1.0.0.1
+# Set CF DNS servers address
+resetprop dhcp.wlan0.dns1 || setprop dhcp.wlan0.dns1 1.1.1.1
+resetprop dhcp.wlan0.dns2 || setprop dhcp.wlan0.dns2 1.0.0.1
 
-setprop net.eth0.dns1 1.1.1.1
-setprop net.eth0.dns2 1.0.0.1
+resetprop net.eth0.dns1 || setprop net.eth0.dns1 1.1.1.1
+resetprop net.eth0.dns2 || setprop net.eth0.dns2 1.0.0.1
 
-setprop setprop net.dns1 1.1.1.1
-setprop setprop net.dns2 1.0.0.1
+resetprop net.dns1 || setprop net.dns1 1.1.1.1
+resetprop net.dns2 || setprop net.dns2 1.0.0.1
 
-setprop net.ppp0.dns1 1.1.1.1
-setprop net.ppp0.dns2 1.0.0.1
+resetprop net.ppp0.dns1 || setprop net.ppp0.dns1 1.1.1.1
+resetprop net.ppp0.dns2 || setprop net.ppp0.dns2 1.0.0.1
 
-setprop net.rmnet0.dns1 1.1.1.1
-setprop net.rmnet0.dns2 1.0.0.1
+resetprop net.rmnet0.dns1 || setprop net.rmnet0.dns1 1.1.1.1
+resetprop net.rmnet0.dns2 || setprop net.rmnet0.dns2 1.0.0.1
 
-setprop net.rmnet1.dns1 1.1.1.1
-setprop net.rmnet1.dns2 1.0.0.1
+resetprop net.rmnet1.dns1 || setprop net.rmnet1.dns1 1.1.1.1
+resetprop net.rmnet1.dns2 || setprop net.rmnet1.dns2 1.0.0.1
 
-setprop net.dns1 1.1.1.1
-setprop net.dns2 1.0.0.1
+resetprop net.pdpbr1.dns1 || setprop net.pdpbr1.dns1 1.1.1.1
+resetprop net.pdpbr1.dns2 || setprop net.pdpbr1.dns2 1.0.0.1
 
-setprop net.pdpbr1.dns1 1.1.1.1
-setprop net.pdpbr1.dns2 1.0.0.1
+
+
+# Edit the resolv conf file if it exist
+
+if [ -a /system/etc/resolv.conf ]; then
+	mkdir -p $MODDIR/system/etc/
+	printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $MODDIR/system/etc/resolv.conf
+	chmod 644 $MODDIR/system/etc/resolv.conf
+fi
