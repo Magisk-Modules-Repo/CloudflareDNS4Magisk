e04f0c1437e15a02d96716ad7dda072c1f19f46a
Rom <skav@gmx.fr>
2018-04-05 13:41:26 +0200

    Enable post-fs-data.sh boot state in config.sh

--- config.sh
+++ config.sh
@@ -28,7 +28,7 @@ AUTOMOUNT=true
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=true
