e47fbcffadddff69c92ca5f97357a8ca50c8f791
Rom <skav@gmx.fr>
2018-04-09 17:29:50 +0200

    Fix config.sh with 2.5

--- config.sh
+++ config.sh
@@ -86,7 +86,6 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
-  set_perm  $MODPATH/system/etc/resolv.conf       0       0       0644
 }
 
 ##########################################################################################
