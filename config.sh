8a711c36959a5658b54116971ffb51278b78dd4b
Rom <skav@gmx.fr>
2018-09-01 12:46:04 +0200

    Update config.sh to 17000

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=false
+AUTOMOUNT=true
 
 # Set to true if you need to load system.prop
 PROPFILE=true
@@ -42,7 +42,7 @@ LATESTARTSERVICE=true
 print_modname() {
   ui_print "*******************************"
   ui_print "     CloudflareDNS4Magisk      "
-  ui_print "            2.7 BETA 2         "
+  ui_print "              2.6              "
   ui_print "     By Rom for Magisk v17+    "
   ui_print "*******************************"
 }
@@ -101,6 +101,8 @@ set_permissions() {
 
 # Edit the resolv conf file if it exist
 resolve_conf() {
+	mount /system 2>/dev/null
+	
 	if [ -a /system/etc/resolv.conf ]; then
 		mkdir -p $MODPATH/system/etc/
 		printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $MODPATH/system/etc/resolv.conf
