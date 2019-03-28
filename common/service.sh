a3726b990e9a6bdf0c55d244a45bb25cdd1940dd
Rom <skav@gmx.fr>
2019-03-29 00:52:36 +0100

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,10 +1,12 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
-# More info in the main Magisk thread
 
 iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
 iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 1.0.0.1:53
