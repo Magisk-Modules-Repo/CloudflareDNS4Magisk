86c5aa2cd253ea1d751dc37a2714c7d35495ebbb
Rom <skav@gmx.fr>
2019-03-29 00:58:54 +0100

    More beautiful

--- install.sh
+++ install.sh
@@ -157,8 +157,8 @@ set_permissions() {
 
 mount /system 2>/dev/null
 	
-	if [ -a /system/etc/resolv.conf ]; then
-		mkdir -p $TMPDIR/system/etc/
-		printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $TMPDIR/system/etc/resolv.conf
-		touch $TMPDIR/auto_mount
-	fi
+if [ -a /system/etc/resolv.conf ]; then
+	mkdir -p $TMPDIR/system/etc/
+	printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $TMPDIR/system/etc/resolv.conf
+	touch $TMPDIR/auto_mount
+fi
